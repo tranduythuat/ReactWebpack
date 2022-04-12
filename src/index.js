11914f6bf4a6c8c38dffb39e87f23ea81e554fec
unknown <thuattran1810@gmail.com>
2022-04-12 22:53:05 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from  "react-dom";
+
+function App() {
+    return (
+        <div>
+            <h1>Xin chao</h1>
+            <h1>Xin test</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
